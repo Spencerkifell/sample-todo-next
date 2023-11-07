b8e9e7da338dd167b38dd2ffb3727dad5f5a7f9f
Spencer Kifell <spencerkfl@gmail.com>
2023-11-07 13:22:16 -0500

    Sample next.js application based off a guide to better understand the framework

---
+++ src/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+const globalForPrisma = global as unknown as {
+    prisma: PrismaClient | undefined
+}
+
+// Instantiates Singleton PrismaClient
+export const prisma = 
+    globalForPrisma.prisma ??
+    new PrismaClient({
+        log: ["query"],
+    });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
